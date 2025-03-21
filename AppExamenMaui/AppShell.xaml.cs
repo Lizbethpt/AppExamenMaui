0c2bff49a33c4b1788a5b3bd1b66fa4e8225013f
LIZBETH PEÑA TORRES <lpena8672@gmail.com>
2025-03-20 19:15:07 -0600

    Agregar archivos de proyecto.

---
+++ AppExamenMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppExamenMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppExamenMaui/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace AppExamenMaui
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
